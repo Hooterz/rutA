47b68fca6ff87306305aa3ef9a990faabf0ea3f1
Sebastian Escudero <sebastianescuderoc@hotmail.com>
2019-11-22 15:02:21 -0600

    Initial Commit

---
+++ rutA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rutA
+//
+//  Created by Sergio R Escudero Buerba on 22/11/19.
+//  Copyright © 2019 Sebastian Escudero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
