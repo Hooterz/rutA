791f8219e43b1fde9e68c860e374cf7b6ec24ff6
Sebastian Escudero <sebastianescuderoc@hotmail.com>
2019-11-28 10:08:07 -0500

    rutA
    asdasdas
    
    Co-Authored-By: aleortega849 <aleortega849@users.noreply.github.com>

--- rutA/ContentView.swift
+++ rutA/ContentView.swift
@@ -8,14 +8,129 @@
 
 import SwiftUI
 
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+let storedMatricula = "123"
+let storedPassword = "123"
+
 struct ContentView: View {
+    
+    @State var matricula: String = ""
+    @State var password: String = ""
+    
+    @State var authenticationFailed: Bool = false
+    @State var authenticationSucceed: Bool = false
+    
+    @State var editingMode: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack {
+                WelcomeText()
+                logotipo_anahuac()
+                MatriculaField(matricula: $matricula, editingMode: $editingMode)
+                PasswordField(password: $password)
+                if authenticationFailed {
+                    Text("Información no coincide. Intentele nuevamente.")
+                        .offset(y: -10)
+                        .foregroundColor(.red)
+                }
+                Button(action: {
+                        if self.matricula == storedMatricula && self.password == storedPassword {
+                            self.authenticationSucceed = true
+                            self.authenticationFailed = false
+                        } else {
+                            self.authenticationFailed = true
+                            self.authenticationSucceed = false
+                        }
+                })
+                {
+                    ContenidoBotonLogin()
+                }
+            }
+            .padding()
+            if authenticationSucceed {
+                 Text("Ingreso exitoso!")
+                    .font(.headline)
+                    .frame(width: 250, height: 80)
+                    .background(Color.green)
+                    .cornerRadius(20.0)
+                    .foregroundColor(.white)
+                    .animation(Animation.default)
+            }
+        }
+        .offset(y: editingMode ? -150 : 0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WelcomeText: View {
+    var body: some View {
+        Text("Bienvenido a")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct logotipo_anahuac: View {
+    var body: some View {
+        Image("logotipo_anahuac")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 350, height: 150)
+            .clipped()
+            //.cornerRadius(150)
+            .padding(.bottom, 75)
+    }
+}
+
+struct ContenidoBotonLogin: View {
+    var body: some View {
+        Text("INGRESAR")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.orange)
+            .cornerRadius(15.0)
+    }
+}
+
+struct MatriculaField: View {
+    
+    @Binding var matricula: String
+    
+    @Binding var editingMode: Bool
+    
+    var body: some View {
+        TextField("Matricula", text: $matricula, onEditingChanged: {edit in
+            if edit == true
+            {self.editingMode = true}
+            else
+            {self.editingMode = false}
+        })
+        .padding()
+        .background(lightGreyColor)
+        .cornerRadius(5.0)
+        .padding(.bottom, 20)
+    }
+}
+
+struct PasswordField: View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        TextField("Contraseña", text: $password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
     }
 }
